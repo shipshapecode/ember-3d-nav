e2b75d8c0d2750a47e457cc5d3d345cefe9b9b58
Robert Wagner <rwagner@jibe.com>
2016-02-23 13:42:59 -0500

    Add a simple test

---
+++ tests/acceptance/ember-3d-nav-test.js
@@ -0,0 +1,17 @@
+import {test} from 'qunit';
+import moduleForAcceptance from '../helpers/module-for-acceptance';
+
+moduleForAcceptance('Nav menu open/close/select behavior');
+
+test("clicking menu button opens nav", function (assert) {
+  assert.expect(2);
+
+  visit('/');
+  andThen(function () {
+    assert.equal(find('.cd-3d-nav-container').hasClass('nav-is-visible'), false, "nav-is-visible class not applied initially");
+  });
+  click('.cd-3d-nav-trigger');
+  andThen(function () {
+    assert.equal(find('.cd-3d-nav-container').hasClass('nav-is-visible'), true, "nav-is-visible class applied after clicking menu button");
+  });
+});
