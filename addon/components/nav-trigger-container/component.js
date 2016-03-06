2c0fe2ac2e4a0511e1960eb4d28b9402e0ab1b1f
Robert Wagner <rwwagner@vt.edu>
2016-03-06 09:52:57 -0500

    Support fixed header

--- addon/components/nav-trigger-container/component.js
+++ addon/components/nav-trigger-container/component.js
@@ -3,6 +3,8 @@ import layout from './template';
 
 export default Ember.Component.extend({
   layout,
+  tagName: 'header',
+  classNameBindings: [':cd-header', 'navIsVisible:nav-is-visible', 'fixed:fixed'],
   actions: {
     toggleMenu(){
       this.sendAction('toggleMenu');
--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,34 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  links: Ember.A([
-    {
-      href: '#0',
-      selected: true,
-      text: 'About'
-    },
-    {
-      href: '#0',
-      selected: false,
-      text: 'Technologies'
-    },
-    {
-      href: '#0',
-      selected: false,
-      text: 'Team'
-    },
-    {
-      href: '#0',
-      selected: false,
-      text: 'Portfolio'
-    },
-    {
-      href: '#0',
-      selected: false,
-      text: 'Contact'
-    }
-  ]),
-  setupController(controller){
-    controller.set('links', this.get('links'));
+  model(){
+    return {
+      links: Ember.A([
+        {
+          href: '#0',
+          selected: true,
+          text: 'About'
+        },
+        {
+          href: '#0',
+          selected: false,
+          text: 'Technologies'
+        },
+        {
+          href: '#0',
+          selected: false,
+          text: 'Team'
+        },
+        {
+          href: '#0',
+          selected: false,
+          text: 'Portfolio'
+        },
+        {
+          href: '#0',
+          selected: false,
+          text: 'Contact'
+        }
+      ])
+    };
   }
 });
