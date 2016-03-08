ee6827fb9c39c9111696a3a8aed7aaa650ce8549
Robert Wagner <rwwagner@vt.edu>
2016-03-07 21:27:43 -0500

    Remove unnecessary selected

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -6,27 +6,22 @@ export default Ember.Route.extend({
       links: Ember.A([
         {
           href: '#0',
-          selected: true,
           text: 'About'
         },
         {
           href: '#0',
-          selected: false,
           text: 'Technologies'
         },
         {
           href: '#0',
-          selected: false,
           text: 'Team'
         },
         {
           href: '#0',
-          selected: false,
           text: 'Portfolio'
         },
         {
           href: '#0',
-          selected: false,
           text: 'Contact'
         }
       ])
