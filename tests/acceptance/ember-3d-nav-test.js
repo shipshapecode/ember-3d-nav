9589656cf76dc8d4a1947d7fc8209349ba57b9e1
Robert Wagner <rwwagner@vt.edu>
2016-03-08 21:50:19 -0500

    Test clicking nav item

--- tests/acceptance/ember-3d-nav-test.js
+++ tests/acceptance/ember-3d-nav-test.js
@@ -1,17 +1,38 @@
+import Ember from 'ember';
 import {test} from 'qunit';
 import moduleForAcceptance from '../helpers/module-for-acceptance';
 
 moduleForAcceptance('Nav menu open/close/select behavior');
 
-test("clicking menu button opens nav", function (assert) {
+test("clicking menu button opens nav", function(assert) {
   assert.expect(2);
 
   visit('/');
-  andThen(function () {
+  andThen(function() {
     assert.equal(find('.cd-3d-nav-container').hasClass('nav-is-visible'), false, "nav-is-visible class not applied initially");
   });
   click('.nav-trigger');
-  andThen(function () {
+  andThen(function() {
     assert.equal(find('.cd-3d-nav-container').hasClass('nav-is-visible'), true, "nav-is-visible class applied after clicking menu button");
+  });
+});
+
+test("clicking an option selects it and closes the menu", function(assert) {
+  assert.expect(4);
+
+  visit('/');
+  andThen(function() {
+    assert.equal(find('.cd-3d-nav-container').hasClass('nav-is-visible'), false, "nav-is-visible class not applied initially");
+  });
+  click('.nav-trigger');
+  andThen(function() {
+    assert.equal(find('.cd-3d-nav-container').hasClass('nav-is-visible'), true, "nav-is-visible class applied after clicking menu button");
+  });
+  Ember.run.scheduleOnce('afterRender', this, function(){
+    click('centered:nth-of-type(2)');
+    Ember.run.scheduleOnce('afterRender', this, function(){
+      assert.equal(find('centered:nth-of-type(2)').hasClass('is-selected'), true, "nav item is selected");
+      assert.equal(find('.cd-3d-nav-container').hasClass('nav-is-visible'), false, "nav-is-visible class removed after clicking nav item");
+    });
   });
 });
