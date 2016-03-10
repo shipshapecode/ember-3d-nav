11ca61e2d193a6c8d5c9453f2faa015b215f80f6
Robert Wagner <rwwagner@vt.edu>
2016-03-09 20:15:04 -0500

    Setup scroll handler

--- addon/components/nav-trigger-container/component.js
+++ addon/components/nav-trigger-container/component.js
@@ -1,11 +1,25 @@
 import Ember from 'ember';
+import $ from 'jquery';
 import layout from './template';
+import RespondsToScroll from 'ember-responds-to/mixins/responds-to-scroll';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(RespondsToScroll, {
   layout,
   tagName: 'header',
+  classNameBindings: [':cd-header', 'navService.navIsVisible:nav-is-visible', 'fixed:fixed', 'fixedAndScrolled'],
   navService: Ember.inject.service('ember-3d-nav'),
-  classNameBindings: [':cd-header', 'navService.navIsVisible:nav-is-visible', 'fixed:fixed'],
+  fixedAndScrolled: false,
+  onScroll: Ember.on('scroll', function() {
+    if (this.get('fixed')) {
+      const scrollPosition = $(window).scrollTop();
+      if (scrollPosition > 0) {
+        this.set('fixedAndScrolled', true);
+      }
+      else if (scrollPosition === 0) {
+        this.set('fixedAndScrolled', false);
+      }
+    }
+  }),
   actions: {
     toggleMenu() {
       this.toggleProperty('navService.navIsVisible');
