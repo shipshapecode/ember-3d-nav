1e6aa80192236a732312ab97a00cde8dbd0908e9
Robert Wagner <rwwagner90@gmail.com>
2016-07-27 09:46:44 -0400

    Update ghpages

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -3,7 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
-    environment: environment,
+    environment,
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
@@ -40,8 +40,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.baseURL = '/ember-3d-nav/';
-
+    ENV.rootURL = '/ember-3d-nav/';
   }
 
   return ENV;
