7d192d7a6e4a42d4846fcf41d4311443645e263f
Robert Wagner <rwwagner90@gmail.com>
2017-03-21 19:52:56 -0400

    Use node assets for headroom

--- index.js
+++ index.js
@@ -3,10 +3,23 @@
 
 module.exports = {
   name: 'ember-3d-nav',
+
+  options: {
+    nodeAssets: {
+      'headroom.js': {
+        vendor: {
+          srcDir: 'dist',
+          destDir: 'headroom',
+          include: ['headroom.min.js']
+        }
+      }
+    }
+  },
+
   included() {
     this._super.included.apply(this, arguments);
     if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
-      this.import('vendor/headroom.min.js');
+      this.import('vendor/headroom/headroom.min.js');
     }
   }
 };
