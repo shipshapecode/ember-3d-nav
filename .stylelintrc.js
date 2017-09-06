3121fee2367811eca7139f595379a423cfa68681
Robert Wagner <rwwagner90@gmail.com>
2017-09-06 11:43:36 -0400

    Update stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  extends: 'stylelint-config-ship-shape',
-  plugins: [
-    'stylelint-scss'
-  ],
-  rules: {
-    'at-rule-empty-line-before': ['always', {
-      except: [
-        'blockless-after-same-name-blockless',
-        'first-nested'
-      ],
-      ignore: ['after-comment'],
-      ignoreAtRules: ['else']
-    }],
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true
-  }
+  extends: 'stylelint-config-ship-shape'
 };
