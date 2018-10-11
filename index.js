52d23941c95fe355f2393068e7d6e3714aaeb09f
Robert Wagner <rwwagner90@gmail.com>
2018-10-11 08:28:24 -0400

    Remove tailwind

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 'use strict';
 
-const MergeTrees = require('broccoli-merge-trees');
-const buildTailwind = require('ember-cli-tailwind/lib/build-tailwind');
-
 module.exports = {
-  name: 'ember-3d-nav',
-
-  config() {
-    return {
-      'ember-cli-tailwind': {
-        shouldBuildTailwind: false
-      }
-    };
-  },
-
-  treeForAddonStyles(tree) {
-    let trees = tree ? [ tree ] : [];
-
-    trees.push(buildTailwind(this));
-
-    return new MergeTrees(trees);
-  }
+  name: 'ember-3d-nav'
 };
